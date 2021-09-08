2922147258bd4c4a9dd9212055918a98d2186849
luisobregon21 <luisitonicaragua2002@gmail.com>
2021-09-08 21:16:57 +0000

    New task added

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,11 @@
+def fizzbuzz():
+    for num in range(1, 100):
+        if (num % 3) == 0 and (num % 5) == 0:
+            print("fizzbuzz ", end='')
+        elif (num % 3) == 0:
+            print("Fizz ", end='')
+        elif (num % 5) == 0:
+            print("Buzz ", end='')
+        else:
+            print("{:d} ".format(num), end='')
+    print("Buzz", end='')
