6540169084e696930d4788c89d6ccff960824a62
luisobregon21 <luisitonicaragua2002@gmail.com>
2021-09-08 18:42:24 +0000

    Changed second elseif number

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -5,9 +5,10 @@ if number < 0:
     num = number % -10
 else:
     num = number % 10
+
 if num > 5:
     print("Last digit of {:d}, is {:d} greater than 5".format(number, num))
 elif num == 0:
     print("Last digit of {:d}, is {:d} and is 0".format(number, number % 10))
-elif num < 6 and num != 0:
+else:
     print("Last digit of {:d}, is {:d} and is less than 6 and not 0".format(number, num))
