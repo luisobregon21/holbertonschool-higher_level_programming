e9bd2b8a615e753316c3b84738c28fd439e7f693
luisobregon21 <luisitonicaragua2002@gmail.com>
2021-09-08 19:53:00 +0000

    Added space in line 3:24

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for num in range(9):
-    for num2 in range(1,10):
+    for num2 in range(1, 10):
         if num < num2:
             print("{:d}{:d}".format(num, num2), end='')
             if num != 8 or num2 != 9:
