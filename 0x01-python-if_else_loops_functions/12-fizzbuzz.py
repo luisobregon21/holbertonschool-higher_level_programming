a8dd505eec6573f372cee707994f55542aab71f7
luisobregon21 <luisitonicaragua2002@gmail.com>
2021-09-08 21:28:01 +0000

    removed last line

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def fizzbuzz():
     for num in range(1, 101):
-        if (num % 3) == 0 and (num % 5) == 0:
-            print("fizzbuzz ", end='')
-        elif (num % 3) == 0:
-            print("Fizz ", end='')
-        elif (num % 5) == 0:
-            print("Buzz ", end='')
+        if num % 3 == 0 and num % 5 == 0:
+            print("fizzbuzz", end=' ')
+        elif num % 3 == 0:
+            print("Fizz", end=' ')
+        elif num % 5 == 0:
+            print("Buzz", end=' ')
         else:
-            print("{:d} ".format(num), end='')
+            print("{:d}".format(num), end=' ')
