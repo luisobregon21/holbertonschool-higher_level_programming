46f60fa996a0efb395b22c030f324b1ef9515080
luisobregon21 <luisitonicaragua2002@gmail.com>
2021-09-13 14:12:51 +0000

    Added new task

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+import sys
+if __name__ == "__main__":
+    argv = sys.argv
+    leng = len(argv) - 1
+
+    if leng == 0:
+        print("{:d} arguements.".format(leng))
+    elif leng == 1:
+        print("{:d} arguement:".format(leng))
+        print("{:d}: {}".format(leng, argv[1]))
+    else:
+        print("{:d} arguements:".format(leng))
+        for num in range(1, leng + 1):
+            print("{:d}: {}".format(num, argv[num]))
