e9d8861df9b7ea8ee2914e3902fb1d89c9395018
luisobregon21 <luisitonicaragua2002@gmail.com>
2021-09-13 16:08:53 +0000

    Switch if statements order

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,13 +3,12 @@ import sys
 if __name__ == "__main__":
     argv = sys.argv
     leng = len(argv) - 1
-
-    if leng == 0:
+    if leng > 1:
+       print("{:d} arguements:".format(leng))
+       for num in range(1, leng + 1):
+           print("{:d}: {}".format(num, argv[num]))
+    elif leng == 0:
         print("{:d} arguements.".format(leng))
     elif leng == 1:
         print("{:d} arguement:".format(leng))
         print("{:d}: {}".format(leng, argv[1]))
-    else:
-        print("{:d} arguements:".format(leng))
-        for num in range(1, leng + 1):
-            print("{:d}: {}".format(num, argv[num]))
