c739c3421c24169a9573860a47668bb5c7f38eee
luisobregon21 <luisitonicaragua2002@gmail.com>
2021-09-13 16:20:28 +0000

    Fixed spelling mistake

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,12 +3,13 @@ import sys
 if __name__ == "__main__":
     argv = sys.argv
     leng = len(argv) - 1
-    if leng > 1:
-       print("{:d} arguements:".format(leng))
-       for num in range(1, leng + 1):
-           print("{:d}: {}".format(num, argv[num]))
-    elif leng == 0:
-        print("0 arguements.")
+
+    if leng == 0:
+        print("{:d} arguments.".format(leng))
     elif leng == 1:
-        print("{1 arguement:".format(leng))
-        print("{1: {}".format(argv[1]))
+        print("{:d} argument:".format(leng))
+        print("{:d}: {}".format(leng, argv[1]))
+    else:
+        print("{:d} arguments:".format(leng))
+        for num in range(1, leng + 1):
+            print("{:d}: {}".format(num, argv[num]))
