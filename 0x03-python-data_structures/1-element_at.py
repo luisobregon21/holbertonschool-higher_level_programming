44242aa7531e3cb8ae45d8d4091c1e2300cec738
luisobregon21 <luisitonicaragua2002@gmail.com>
2021-09-14 13:05:12 +0000

    Deleted comments

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,10 +1,3 @@
-'''Write a function that retrieves an element from a list like in C.
-
-Prototype: def element_at(my_list, idx):
-If idx is negative, the function should return None
-If idx is out of range (> of number of element in my_list), the function should return None
-You are not allowed to import any module
-You are not allowed to use try/except'''
 #!/usr/bin/python3
 def element_at(my_list, idx):
     if idx < 0 or idx > len(my_list) - 1:
