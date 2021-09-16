bf22f31d826a142534cb6d1c727c834ef4cdf95f
luisobregon21 <luisitonicaragua2002@gmail.com>
2021-09-16 21:34:51 +0000

    Delete spaces in line 3

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,21 +1,13 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    mapping  = {
-        'I': 1,
-        'V': 5,
-        'X': 10,
-        'L': 50,
-        'C': 100,
-        'D': 500,
-        'M': 1000,
-        }
-    min_ = None
+    roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500}
+    num = None
     total = 0
     for c in roman_string:
-        val = mapping[c]
-        if min_ and val > min_:
-            total -= min_*2
+        val = roman[c]
+        if num and val > num:
+            total -= num * 2
         else:
-            min_ = val
+            num = val
         total += val
     return total
