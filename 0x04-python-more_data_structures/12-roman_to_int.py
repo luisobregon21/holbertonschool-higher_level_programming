20dc5c5335efd8318bd36f3bb5a731e2a5084958
luisobregon21 <luisitonicaragua2002@gmail.com>
2021-09-16 21:47:01 +0000

    Add string check condition

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
+    if roman_string is None or not isinstance(roman_string, str):
+        return 0
     roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500}
     num = None
     total = 0
