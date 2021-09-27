b15a07eacbe42102361e96acda96decb8886a0bf
luis <luisitonicaragua2002@gmail.com>
2021-09-27 16:04:18 +0000

    Rearrange try and except

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     count = 0
-    try:
-        for i in range(x):
+    for i in range(x):
+        try:
             print("{}".format(my_list[i]), end="")
             count += 1
-        print()
-    except:
-        return count
+        except:
+            continue
+    print()
     return count
