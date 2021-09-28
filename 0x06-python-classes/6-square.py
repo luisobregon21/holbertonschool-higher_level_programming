d8cb44300a3768b56070e19b773a6ba1ef4685d4
luisobregon21 <luisitonicaragua2002@gmail.com>
2021-09-28 23:07:57 +0000

    Add loop for position[1]

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -28,10 +28,16 @@ class Square:
     @position.setter
     def position(self, value):
         """ function to set value of __position """
-        if type(value) is not int and value < (0, 0):
+        if type(value) is not tuple or len(value) != 2:
             raise TypeError("position must be a tuple of 2 positive integers")
-        else:
-            self.__position = value
+
+        if type(value[0]) is not int or type(value[1]) is not int:
+            raise TypeError("position must be a tuple of 2 positive integers")
+
+        if value[0] < 0 or value[1] < 0:
+            raise TypeError("position must be a tuple of 2 positive integers")
+
+        self.__position = value
 
     @property
     def size(self):
