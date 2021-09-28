dc4cf0c8e1b19791113db5f4a699205c72d17607
luisobregon21 <luisitonicaragua2002@gmail.com>
2021-09-28 23:23:20 +0000

    Ugly if

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -33,7 +33,7 @@ class Square:
         if type(value) is not tuple or len(value) != 2 or\
            type(value[0]) is not int or type(value[1]) is not int or\
            value[0] < 0 or value[1] < 0:
-               raise TypeError(x)
+            raise TypeError(x)
         else:
             self.__position = value
 
