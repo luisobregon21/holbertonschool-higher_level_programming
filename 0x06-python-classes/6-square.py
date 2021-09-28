d9c00f091717148693561776be5a27c5472c2f65
luisobregon21 <luisitonicaragua2002@gmail.com>
2021-09-28 23:22:48 +0000

    Ugly if

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -30,12 +30,10 @@ class Square:
         """ function to set value of __position """
         x = "position must be a tuple of 2 positive integers"
 
-        if type(value) is not tuple or len(value) != 2:
-            raise TypeError(x)
-        elif type(value[0]) is not int or type(value[1]) is not int:
-            raise TypeError(x)
-        elif value[0] < 0 or value[1] < 0:
-            raise TypeError(x)
+        if type(value) is not tuple or len(value) != 2 or\
+           type(value[0]) is not int or type(value[1]) is not int or\
+           value[0] < 0 or value[1] < 0:
+               raise TypeError(x)
         else:
             self.__position = value
 
