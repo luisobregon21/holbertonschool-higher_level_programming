369016754f4cca395ec3c3030aae1a6bbcc353d5
luisobregon21 <luisitonicaragua2002@gmail.com>
2021-09-28 22:31:03 +0000

    Add new task

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -57,7 +57,7 @@ class Square:
         if self.__size == 0:
             print()
         for i in range(self.__size):
-            if (self.__position[1] == 0):
+            if (self.__position[1] <= 0):
                 for space in range(self.__position[0]):
                     print(" ", end="")
             for j in range(self.__size):
