f1c97179af9a5bd3d5d03dfcec95b7a2bce1fdcb
luisobregon21 <luisitonicaragua2002@gmail.com>
2021-09-28 00:47:53 +0000

    Add new task

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import sys
+
+
+def safe_function(fct, *args):
+    try:
+        result = fct(*args)
+        return result
+    except Exception as err:
+        print("Exception:", err, file=sys.stderr)
+        return None
