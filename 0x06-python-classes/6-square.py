e44d9cc499fe672941cdd6ae3729aa568759d96c
luisobregon21 <luisitonicaragua2002@gmail.com>
2021-09-28 23:28:20 +0000

    PEP8 indentation

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -35,6 +35,7 @@ class Square:
                 self.__position = value
         else:
             raise TypeError("position must be a tuple of 2 positive integers")
+
     @property
     def size(self):
         """ function to get value of __size """
