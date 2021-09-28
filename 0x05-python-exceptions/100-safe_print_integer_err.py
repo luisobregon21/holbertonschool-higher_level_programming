193b0b121b423f0d83a559ff4fc538549fb24ed1
luisobregon21 <luisitonicaragua2002@gmail.com>
2021-09-28 00:40:08 +0000

    Add new task

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 import sys
+
+
 def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
         return True
     except Exception as err:
-        print("Exception:", err, file = sys.stderr)
+        print("Exception:", err, file=sys.stderr)
         return False
