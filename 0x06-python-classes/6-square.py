eff1b99e35234584c6149f2a8ac831c00c75da92
luisobregon21 <luisitonicaragua2002@gmail.com>
2021-09-28 23:12:49 +0000

    Tie all ifs for position down

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -28,16 +28,16 @@ class Square:
     @position.setter
     def position(self, value):
         """ function to set value of __position """
+        x = "position must be a tuple of 2 positive integers"
+
         if type(value) is not tuple or len(value) != 2:
-            raise TypeError("position must be a tuple of 2 positive integers")
-
-        if type(value[0]) is not int or type(value[1]) is not int:
-            raise TypeError("position must be a tuple of 2 positive integers")
-
-        if value[0] < 0 or value[1] < 0:
-            raise TypeError("position must be a tuple of 2 positive integers")
-
-        self.__position = value
+            raise TypeError(x)
+        elif type(value[0]) is not int or type(value[1]) is not int:
+            raise TypeError(x)
+        elif value[0] < 0 or value[1] < 0:
+            raise TypeError(x)
+        else:
+            self.__position = value
 
     @property
     def size(self):
