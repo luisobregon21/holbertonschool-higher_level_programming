a9310d2561d197a485468b1cee6daac6cae2b73b
luisobregon21 <luisitonicaragua2002@gmail.com>
2021-10-05 23:37:11 +0000

    Add new task

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -1,5 +1,44 @@
 #!/usr/bin/python3
-''' UNITESTS for the function max_integer '''
+"""Unittest for max_integer([..])
+"""
+import unittest
+max_integer = __import__('6-max_integer').max_integer
 
-def max_integer(list=[]):
-    ''' '''
+class TestMaxInteger(unittest.TestCase):
+    ''' Class tests max integer function'''
+    def test_max(self):
+        ''' Tests for regular max in list '''
+        l = [1, 2, 3, 4]
+        self.assertEqual(max_integer(l), 4)
+
+        l = [1, 2, 4, 8]
+        self.assertEqual(max_integer(l), 8)
+
+        l = [1, 22, 3, 4]
+        self.assertEqual(max_integer(l), 22)
+
+        l = [1, 2, 3.5, 4]
+        self.assertEqual(max_integer(l), 4)
+
+        l = [-5, -2, 8, -7]
+        self.assertEqual(max_integer(l), 8)
+
+        l = [None]
+        self.assertEqual(max_integer(l), None)
+
+        l = [100, 2, 3, 4]
+        self.assertEqual(max_integer(l), 100)
+
+        l = [6, 2, 3, 14, 8, 9, 7]
+        self.assertEqual(max_integer(l), 14)
+
+        l = [-1, -2, -3, -4]
+        self.assertEqual(max_integer(l), -1)
+
+        l = [3]
+        self.assertEqual(max_integer(l), 3)
+
+        self.assertEqual(max_integer([]), None)
+
+if __name__ == "__main__":
+    unittest.main()
