35f04979a3833a88d558fc43fd93f45ab591f22c
luis <luisitonicaragua2002@gmail.com>
2021-10-12 15:30:32 +0000

    Add new task

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+''' Module has function that checks for the instance of a class '''
+
+
+def inherits_from(obj, a_class):
+    ''' return True/False if obj is instance of a class
+        that inhereted specified class '''
+
+    if type(obj) != a_class:
+        return True
+    else:
+        return False
