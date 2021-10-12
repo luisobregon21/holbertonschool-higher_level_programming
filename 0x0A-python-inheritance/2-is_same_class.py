69d2fc96c6fe06d2d09df39a455fa9996669be70
luis <luisitonicaragua2002@gmail.com>
2021-10-12 15:18:13 +0000

    Change isintance() for type()

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -11,4 +11,7 @@ def is_same_class(obj, a_class):
             @class: specified class
     '''
 
-    return isinstance(obj, a_class)
+    if type(obj) == a_class:
+        return True
+    else:
+        return False
