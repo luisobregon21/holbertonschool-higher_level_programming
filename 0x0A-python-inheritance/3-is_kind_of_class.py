aa6382d178d511fc39ed537a9060f2f76268a177
luis <luisitonicaragua2002@gmail.com>
2021-10-12 15:22:24 +0000

    Add new task

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+''' Module has function that checks for object instance '''
+
+
+def is_kind_of_class(obj, a_class):
+    ''' returns True/False for object instace '''
+    return isinstance(obj, a_class)
