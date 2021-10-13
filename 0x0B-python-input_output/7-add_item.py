4ed767bd532895199455d70221dceeb3f5877a6e
luisobregon21 <luisitonicaragua2002@gmail.com>
2021-10-13 22:00:02 +0000

    Add try and except

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -3,14 +3,20 @@
     and then save them to a file '''
 
 import json
-import sys
+from sys import argv
 
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 pythonlist = []
+jasonfile = "add_item.json"
 
-for a in range(1, len(sys.argv)):
-    pythonlist.append(sys.argv[a])
 
-save_to_json_file(pythonlist, "add_item.json")
+try:
+    pythonlist = load_from_json_file(jasonfile)
+except:
+    pass
+for a in range(1, len(argv)):
+    pythonlist.append(argv[a])
+
+save_to_json_file(pythonlist, jasonfile)
