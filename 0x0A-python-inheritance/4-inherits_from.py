caeb0e7f524ae71612f5d558cc5d9a45b999972c
luisobregon21 <luisitonicaragua2002@gmail.com>
2021-10-13 00:25:17 +0000

    Add isinstance

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -6,7 +6,7 @@ def inherits_from(obj, a_class):
     ''' return True/False if obj is instance of a class
         that inhereted specified class '''
 
-    if type(obj) != a_class:
+    if type(obj) != a_class or isinstance(obj, a_class) is True:
         return True
     else:
         return False
