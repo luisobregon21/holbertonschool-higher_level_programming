fee11d59c98928bc6a963a0ed5054b4efe4e26e8
luisobregon21 <luisitonicaragua2002@gmail.com>
2021-10-13 21:51:45 +0000

    Add new task

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -3,14 +3,14 @@
     and then save them to a file '''
 
 import json
-from sys import argv
+from sys
 
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 pythonlist = []
 
-for a in range(1, len(argv)):
-    pythonlist.append(argv[a])
+for a in range(1, len(sys.argv)):
+    pythonlist.append(sys.argv[a])
 
 save_to_json_file(pythonlist, "add_item.json")
