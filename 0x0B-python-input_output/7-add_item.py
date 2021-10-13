f5bb0475cdbf20efc70646137c6b20bacc87dc9f
luisobregon21 <luisitonicaragua2002@gmail.com>
2021-10-13 21:52:15 +0000

    Add new task

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -3,7 +3,7 @@
     and then save them to a file '''
 
 import json
-from sys
+import sys
 
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
