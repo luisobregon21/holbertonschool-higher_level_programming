37b2dfb69436928c59c67e903462fbcfdf659d07
luisobregon21 <luisitonicaragua2002@gmail.com>
2021-10-13 00:21:46 +0000

    Add __eq__ and __ne__

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -4,6 +4,10 @@
 
 class MyInt(int):
     ''' class inherits from int '''
-    def __init__(self, num):
-        ''' comment '''
-        self.num = num
+    def __eq__(self, other):
+        ''' reverses __eq__ logic '''
+        return (self.real != other.real) or (self.imag != other.imag)
+
+    def __ne__(self, other):
+        ''' reverses __ne__ logic '''
+        return (self.real == other.real) and (self.imag == other.imag)
