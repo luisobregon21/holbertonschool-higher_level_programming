af62c482980b08df044472cede0df26682d25950
luisobregon21 <luisitonicaragua2002@gmail.com>
2021-11-17 20:06:21 +0000

    Add new task

---
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -0,0 +1,5 @@
+-- lists all privileges of the MySQL users
+-- user_0d_1 and user_0d_2 in the localhost Server
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+-- GRANTS shows privileges for a specific MySQL user
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
