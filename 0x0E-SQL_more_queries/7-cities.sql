5d10bb8b3475ac380f3e03b8e10b479da9f7b0c5
luisobregon21 <luisitonicaragua2002@gmail.com>
2021-11-17 21:54:56 +0000

    Add comments for FOREIGN KEY usage

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -5,8 +5,12 @@ CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 USE hbtn_0d_usa;
 -- Create the table cities
 CREATE TABLE IF NOT EXISTS cities(
+-- AUTO_INCREMENT generate a unique identity for new rows:
 	id INT PRIMARY KEY AUTO_INCREMENT,
 	state_id INT NOT NULL,
+-- foreign key is a constraint that links a column in one table
+-- to a column in a different table and ensures that a value can be added
+-- to column_a only if the same value already exists in column_b.
 	FOREIGN KEY(state_id) REFERENCES states(id),
 	name VARCHAR(256) NOT NULL
 );
