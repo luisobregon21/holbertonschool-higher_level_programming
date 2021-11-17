6f4274d256f5f203f60faf6c964df25b6360fcf6
luisobregon21 <luisitonicaragua2002@gmail.com>
2021-11-17 01:40:59 +0000

    add new task

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,5 @@
+-- lists all records of the table second_table
+-- of the database hbtn_0c_0 in your MySQL server.
+-- Results are displays both the score and the name (in this order)
+-- Records are ordered by score (top first)
+SELECT score, name FROM second_table order by score DESC;
