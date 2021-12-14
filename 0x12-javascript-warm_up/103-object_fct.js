2e966d961ed05f64c38dd4842386e1b1ab9b5441
luisobregon21 <luisitonicaragua2002@gmail.com>
2021-12-14 01:24:51 +0000

    Delete spaces

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -5,12 +5,8 @@ const myObject = {
   type: 'object',
   value: 12
 };
-
 console.log(myObject);
-myObject.incr = function () {
-  this.value++;
-};
-
+myObject.incr = function () { this.value++; };
 myObject.incr();
 console.log(myObject);
 myObject.incr();
