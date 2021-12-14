0dc3d145c61b4949de111baa8585e49d0205d78d
luisobregon21 <luisitonicaragua2002@gmail.com>
2021-12-14 01:21:20 +0000

    add new task

---
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+// function incr that increments the integer value.
+
+const myObject = {
+  type: 'object',
+  value: 12
+};
+
+console.log(myObject);
+myObject.incr = function () {
+  this.value++;
+};
+
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
