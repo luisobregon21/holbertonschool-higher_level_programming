c990e0f2caa1cf24b6817f0c6f4bcd5f8b904b66
luis <luisitonicaragua2002@gmail.com>
2021-12-15 20:51:02 +0000

    Add Last two advanced task

---
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+//Computes a dictionary of user ids by occurrence.
+
+// rquire() is a built-in function with a special purpose: to load modules.
+const dict = require('./101-data').dict;
+const newdict = {};
+for (const key in dict) {
+  if (newdict[dict[key]] === undefined) {
+    newdict[dict[key]] = [];
+  }
+  newdict[dict[key]].push(key);
+}
+
+console.log(newdict);
---
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+// that concats 2 files.
+
+const fileA = process.argv[2];
+const fileB = process.argv[3];
+const fileC = process.argv[4];
+const fs = require('fs');
+const txtA = fs.readFileSync(fileA);
+const txtB = fs.readFileSync(fileB);
+fs.writeFileSync(fileC, txtA + txtB);
