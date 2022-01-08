cac65e1138668a4c5460072391e078bf15c97002
luisobregon21 <luisitonicaragua2002@gmail.com>
2022-01-08 21:33:28 +0000

    Add grep and cut

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # takes in a URL and displays all HTTP methods the server will accept.
-curl -sI "$1" | grep -i Allow | cut -d ' ' -f 2
+curl -sI "$1" | grep -i Allow | cut -d ' ' -f 2-
