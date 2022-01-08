aac8428b62b8117000ac459bb91a7a0d23894413
luisobregon21 <luisitonicaragua2002@gmail.com>
2022-01-08 21:39:58 +0000

    Add GET and -X option

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # script takes in a URL as an argument, sends a GET request to the URL, and displays the body of the response
-curl -s "$1" -H "X-HolbertonSchool-User-Id: 98"
+curl -s -X GET -H "X-HolbertonSchool-User-Id: 98" "$1"
