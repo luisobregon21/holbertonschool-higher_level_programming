d953aabb817b8d5209d516bc9ea1acb57494e8f5
luisobregon21 <luisitonicaragua2002@gmail.com>
2022-01-08 23:38:44 +0000

    add if main

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -5,10 +5,11 @@
 import urllib.request
 
 
-# opens URL
-with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
-    html = response.read()
-    print("Body response:")
-    print("\t- type: {}".format(type(html)))
-    print("\t- content: {}".format(html))
-    print("\t- utf8 content: {}".format(html.decode('utf8')))
+if __name__ == '__main__':
+    # opens URL
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+        html = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(html)))
+        print("\t- content: {}".format(html))
+        print("\t- utf8 content: {}".format(html.decode('utf8')))
