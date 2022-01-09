dd6d6bbb5c988f1d6f0c9208fecc889c83060f51
luisobregon21 <luisitonicaragua2002@gmail.com>
2022-01-09 02:24:47 +0000

    Testing format

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -8,7 +8,5 @@ import urllib.request
 # opens URL
 with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
     html = response.read()
-    print("Body response:")
-    print("    - type: {}".format(type(html)))
-    print("    - content: {}".format(html))
-    print("    - utf8 content: {}".format(html.decode('utf8')))
+    r = "Body response:\n\t- type: {}\n\t- content: {}\n\t- utf8 content: {}"
+    print(r.format(type(html), html, html.decode('utf8')))
