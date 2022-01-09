c36f8860d8ea492cd1c44e544436958a44066445
luisobregon21 <luisitonicaragua2002@gmail.com>
2022-01-09 02:21:29 +0000

    rremove tab and exchanged it for spaces

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -9,6 +9,6 @@ import urllib.request
 with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
     html = response.read()
     print("Body response:")
-    print("   - type: {}".format(type(html)))
-    print("   - content: {}".format(html))
-    print("   - utf8 content: {}".format(html.decode('utf8')))
+    print("    - type: {}".format(type(html)))
+    print("    - content: {}".format(html))
+    print("    - utf8 content: {}".format(html.decode('utf8')))
