0a295f4222664df7e9de5936424fc90df19de1eb
luisobregon21 <luisitonicaragua2002@gmail.com>
2022-01-09 02:25:42 +0000

    Reverting changes

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -8,5 +8,7 @@ import urllib.request
 # opens URL
 with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
     html = response.read()
-    r = "Body response:\n\t- type: {}\n\t- content: {}\n\t- utf8 content: {}"
-    print(r.format(type(html), html, html.decode('utf8')))
+    print("Body response:")
+    print("\t- type: {}".format(type(html)))
+    print("\t- content: {}".format(html))
+    print("\t- utf8 content: {}".format(html.decode('utf8')))
