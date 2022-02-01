d62dc0e070b44858abdeb3b896f86cccb352372d
Luis Obregon <luisitonicaragua2002@gmail.com>
2022-02-01 11:03:38 -0400

    Fix syntax error

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,14 +1,12 @@
 #!/usr/bin/node
-// script writes a string to a file
-const process = require('process');
-const request = require('request');
+ // script writes a string to a file
+const process = require("process");
+const request = require("request");
 
 if (process.argv.length === 3) {
-	const url = process.argv[2];
-	request
-	  .get(url)
-	  .on('response', function(response) {
-      console.log(`code : ${response.statusCode}`); // 200
-      console.log(response.headers['content-type']); // 'image/png'
-	  }
+    const url = process.argv[2];
+    request.get(url).on("response", function(response) {
+        console.log(`code : ${response.statusCode}`); // 200
+        console.log(response.headers["content-type"]); // 'image/png'
+    });
 }
