dc733410ef73910930eb7641cdcbe34e040806fa
luisobregon21 <luisitonicaragua2002@gmail.com>
2022-02-01 14:54:46 +0000

    Add new task

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+// script writes a string to a file
+const process = require('process');
+const request = require('request');
+
+if (process.argv.length === 3) {
+	const url = process.argv[2];
+	request
+	  .get(url)
+	  .on('response', function(response) {
+      console.log(`code : ${response.statusCode}`); // 200
+      console.log(response.headers['content-type']); // 'image/png'
+	  }
+}
