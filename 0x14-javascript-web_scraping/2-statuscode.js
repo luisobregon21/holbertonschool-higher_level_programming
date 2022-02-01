cdc0008e3a7707f549c62671b4deee10dcf05e7c
luisobregon21 <luisitonicaragua2002@gmail.com>
2022-02-01 15:08:48 +0000

    Edit condtion

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -3,7 +3,7 @@
 const process = require('process');
 const request = require('request');
 
-if (process.argv.length === 3) {
+if (process.argv.length >= 3) {
   const url = process.argv[2];
   request.get(url).on('response', function (response) {
     console.log(`code: ${response.statusCode}`);
