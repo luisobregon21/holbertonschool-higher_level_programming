ce721958b4030570b40418139ce957cdedcab012
luisobregon21 <luisitonicaragua2002@gmail.com>
2022-02-01 17:12:04 +0000

    delete errror

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -5,10 +5,7 @@ const request = require('request');
 
 if (process.argv.length >= 3) {
   const url = process.argv[2];
-	request.get(url, function(error, response) {
-		if (error){
-			console.error('error:', error);
-		}
+	request.get(url, function(response) {
 		else{
 			console.log(`code: ${response.statusCode}`);
 		}
