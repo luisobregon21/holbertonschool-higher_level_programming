7e738d6a88015c1bdf38481018f26dd81276f8fb
Luis Obregon <luisitonicaragua2002@gmail.com>
2022-02-09 21:28:18 -0400

    Fix semistandard

--- 0x15-javascript-web_jquery/9-script.js
+++ 0x15-javascript-web_jquery/9-script.js
@@ -1,3 +1,3 @@
-$.get("https://fourtonfish.com/hellosalut/?lang=fr", function(data) {
-    $("DIV#hello").text(data.hello);
+$.get('https://fourtonfish.com/hellosalut/?lang=fr', function (data) {
+  $('DIV#hello').text(data.hello);
 });
