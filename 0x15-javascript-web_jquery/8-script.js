39cf0087d0636597e222b08c8bfa577cca94ce8d
Luis Obregon <luisitonicaragua2002@gmail.com>
2022-02-09 21:09:07 -0400

    Add task 8 and 9

---
+++ 0x15-javascript-web_jquery/8-script.js
@@ -0,0 +1,5 @@
+$.get("https://swapi-api.hbtn.io/api/films/?format=json", function(movies) {
+    for (const movie of movies.results) {
+        $("UL#list_movies").append("<li>" + movie.title + "</li>");
+    }
+});
